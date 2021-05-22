60d735b762e5f856eb190e3a70dc87e08f7d820e
takahiro okada <t.okada@koshikiad.com>
2021-05-22 12:26:43 +0900

    make link

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
-  def articles
-    @article = Article.first
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
