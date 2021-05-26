70477e7ca09452a2acb296ae378a83fdb61e1aa9
takahiro okada <t.okada@koshikiad.com>
2021-05-26 12:41:19 +0900

    データベースに関連性をもたせる

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_122731) do
+ActiveRecord::Schema.define(version: 2021_05_26_033515) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "models", force: :cascade do |t|
